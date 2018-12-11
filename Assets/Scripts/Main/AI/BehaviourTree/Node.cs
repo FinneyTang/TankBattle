b5e2bbf8c4e0cf640e77c75bc5a7988c7343e6fb
Finney <finneytang@gmail.com>
2018-12-11 22:05:03 +0800

    add bt

---
+++ Assets/Scripts/Main/AI/BehaviourTree/Node.cs
@@ -0,0 +1,49 @@
+﻿using AI.Base;
+using AI.Blackboard;
+using AI.RuleBased;
+using System.Collections.Generic;
+
+namespace AI.BehaviourTree
+{
+    public enum ERunningStatus
+    {
+        Failed, Executing, Finished
+    }
+    public class Node
+    {
+        protected Node m_Parent;
+        protected List<Node> m_Children = new List<Node>();
+
+        private Condition m_Precondition;
+        public ERunningStatus Update(IAgent agent, BlackboardMemory workingMemroy)
+        {
+            if(m_Precondition != null && m_Precondition.IsTrue(agent) == false)
+            {
+                return ERunningStatus.Failed;
+            }
+            return OnUpdate(agent, workingMemroy);
+        }
+        public void Transition(IAgent agent, BlackboardMemory workingMemroy)
+        {
+            OnTransition(agent, workingMemroy);
+        }
+        public Node AddChild(Node c)
+        {
+            c.m_Parent = this;
+            m_Children.Add(c);
+            return this;
+        }
+        public Node SetPrecondition(Condition p)
+        {
+            m_Precondition = p;
+            return this;
+        }
+        protected virtual ERunningStatus OnUpdate(IAgent agent, BlackboardMemory workingMemroy)
+        {
+            return ERunningStatus.Finished;
+        }
+        protected virtual void OnTransition(IAgent agent, BlackboardMemory workingMemroy)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Main/AI/BehaviourTree/SelectorNode.cs
@@ -0,0 +1,45 @@
+﻿using AI.Base;
+using AI.Blackboard;
+
+namespace AI.BehaviourTree
+{
+    public class SelectorNode : Node
+    {
+        private Node m_LastRunningNode;
+        protected override ERunningStatus OnUpdate(IAgent agent, BlackboardMemory workingMemroy)
+        {
+            ERunningStatus runningStatus = ERunningStatus.Failed;
+            Node curNode = null;
+            foreach (Node c in m_Children)
+            {
+                runningStatus = c.Update(agent, workingMemroy);
+                if(runningStatus == ERunningStatus.Failed)
+                {
+                    continue;
+                }
+                else
+                {
+                    curNode = c;
+                    break;
+                }
+            }
+            if (curNode != m_LastRunningNode)
+            {
+                if(m_LastRunningNode != null)
+                {
+                    m_LastRunningNode.Transition(agent, workingMemroy);
+                }
+                m_LastRunningNode = curNode;
+            }
+            return runningStatus;
+        }
+        protected override void OnTransition(IAgent agent, BlackboardMemory workingMemroy)
+        {
+            if (m_LastRunningNode != null)
+            {
+                m_LastRunningNode.Transition(agent, workingMemroy);
+                m_LastRunningNode = null;
+            }
+        }
+    }
+}
